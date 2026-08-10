8c49655854c9dfca512a064259ba527f924d92dc
gloria-byte-mc <gloriamachoka1@gmail.com>
2026-08-10 11:09:40 +0300

    added page links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import WhoWeAre from "./pages/WhoWeAre";
+import Products from "./pages/Products";
+import ContactUs from "./pages/ContactUs";
 import Footer from "./components/Footer";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/who-we-are" element={<WhoWeAre />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
       <Footer />
     </>
